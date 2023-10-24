14d5bd9d5c96fae55929c913978dacfc25fcffff
Wordluc <40872910+Wordluc@users.noreply.github.com>
2023-10-24 22:01:44 +0200

    inserimento lettura file

--- Program.cs
+++ Program.cs
@@ -2,25 +2,31 @@
 
 using System.Data.SqlClient;
 
-async Task<bool> RunQuery(string connectionString, string query)
+IEnumerable<string> GetCommands(string fileName)
+{
+    using (var sr = new StreamReader(fileName))
+    {
+        var command = sr.ReadToEnd().Split(";").ToList();
+        foreach(var c in command)
+            yield return "begin "+c+" commit";
+    }
+}
+
+bool RunCommand(string connectionString, string query)
 {
     using (SqlConnection connection = new SqlConnection(connectionString))
     {
         SqlCommand command = new(query, connection);
         connection.Open();
         SqlDataReader reader = command.ExecuteReader();
-        while (reader.Read())
-        {
-            Console.WriteLine(reader.FieldCount);
-
-        }
-        return reader.FieldCount>0;
+        return reader.RecordsAffected > 0;
     }
-    return false;
 }
 
 
 var your_password = "9cRuYZSj9n0yelbkOcV4f2UAR5";
 var connectionString = $"Server=tcp:lechler-dev-core-azsql-server.database.windows.net,1433;Initial Catalog=lechler_dev_map_ns;Persist Security Info=False;User ID=core-azsql-admin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-RunQuery(connectionString, "select *  from [dbo].[PersonalFormula]");
+foreach (var command in GetCommands(connectionString))
+    Console.WriteLine(command)
+    //Console.Write(RunCommand(connectionString, command));
